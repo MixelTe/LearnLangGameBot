1bcfde95794ac99740d946bfa38e15277f600550
MixelTe <63655075+MixelTe@users.noreply.github.com>
2024-11-24 11:44:41 +0000

    Deploying to gh-pages from @ MixelTe/LearnLangGameBot@ec8836e882927db6738856c5d5f8b9ecda1e9926 🚀

--- alembic/versions/2024_11_24_6db6e2b0ecef_v1.py
+++ alembic/versions/2024_11_24_6db6e2b0ecef_v1.py
@@ -1,8 +1,8 @@
 """v1
 
-Revision ID: ddbd0c866075
+Revision ID: 6db6e2b0ecef
 Revises: 
-Create Date: 2024-11-24 14:26:57.045071
+Create Date: 2024-11-24 14:42:27.095673
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ddbd0c866075'
+revision: str = '6db6e2b0ecef'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
