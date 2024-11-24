26c741b6ef81c52d9cfd8b40ebc62670446a3de1
MixelTe <63655075+MixelTe@users.noreply.github.com>
2024-11-24 12:32:22 +0000

    Deploying to gh-pages from @ MixelTe/LearnLangGameBot@c8378d5a5c1e926dda827f2c1059bd8fca3cf87e 🚀

--- wwwroot/words/ts/tg.d.ts
+++ wwwroot/words/ts/tg.d.ts
@@ -3,6 +3,7 @@ declare global
     const TelegramGameProxy: {
         initParams: any,
     }
+    // https://core.telegram.org/bots/webapps#initializing-mini-apps
     const Telegram: {
         WebApp: {
             initData: string,
